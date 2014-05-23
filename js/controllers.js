a4a683bf53db6fbf91f6bf88f36ad887214f06ee
Kasidis Arunruangsirilert <kken62@gmail.com>
2014-05-23 18:45:08 +0700

    First Commit

---
+++ js/controllers.js
@@ -0,0 +1,64 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope,$http,Parse) {
+      $scope.friends = [];
+  console.log("Working");
+
+  // Is a number
+
+    $scope.btn_search = function(searchBox,callback){
+        console.log(searchBox);
+            if (!isNaN(parseInt(searchBox[0]))) {
+  Parse.searchTel(searchBox,function(data){
+    console.log(data);
+    $scope.friends = data.results;
+  });
+
+}
+else{
+      Parse.searchName(searchBox,function(data){
+    console.log(data);
+    $scope.friends = data.results;
+})
+}
+
+}
+
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams,Parse) {
+      $scope.friends = [];
+  console.log($stateParams.friendId);
+    Parse.searchTel($stateParams.friendId,function(data){
+    console.log(data);
+    $scope.friends = data.results;
+  });
+})
+.controller('PlaylistDetailCtrl', function($scope, $stateParams,Parse) {
+      $scope.friends = [];
+  console.log($stateParams.friendId);
+    Parse.searchHot($stateParams.friendId,function(data){
+    console.log(data);
+    $scope.friends = data.results;
+  });
+})
+.controller('HomeCtrl', function($scope,$http,Parse) {
+  /*$scope.playlists = [
+    { productName: 'Namthip Drinking Water 500ml',price :'7 Baht',store:'Tops',promoString:'Buy 1 Get 1', id: 1 },
+  ];*/
+
+ $scope.friends = [];
+  console.log("Working");
+  Parse.getAll(function(data){
+    console.log(data);
+    $scope.friends = data.results;
+  });
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+.controller('AccountCtrl', function($scope) {
+});
---
+++ js/services.js
@@ -0,0 +1,89 @@
+angular.module('starter.services', [])
+
+.factory('Parse', function ($http) {
+    var app_id = 'syN64QMWju6KQhFZYxbgIvMNfMgKq2DkVb6cHYEi';
+    var api_key = 'HEVdhYLuFSEWHC86GbMVQyPkWziTKGmgwJ2ioF9C';
+    console.log("Factory");
+    return {
+        searchHot : function(q,callback){
+          console.log(q);
+            $http({
+                 params: {
+            where: {barcode:q}
+                },
+            method: 'GET',
+                url:'https://api.parse.com/1/classes/hotDeal',
+                headers:
+                {
+                    'X-Parse-Application-Id': app_id,
+                    'X-Parse-REST-API-Key': api_key,
+                    'Content-Type': 'application/json'
+                }
+            })
+            .success(function(data) {
+                console.log(data);
+                callback(data);
+            });
+        },
+         searchTel : function(q,callback){
+          console.log(q);
+            $http({
+                 params: {
+            where: {barcode:q}
+                },
+            method: 'GET',
+                url:'https://api.parse.com/1/classes/products',
+                headers:
+                {
+                    'X-Parse-Application-Id': app_id,
+                    'X-Parse-REST-API-Key': api_key,
+                    'Content-Type': 'application/json'
+                }
+            })
+            .success(function(data) {
+                console.log(data);
+                callback(data);
+            });
+        },
+           searchName : function(q,callback){
+            console.log(q);
+            $http({
+            method: 'GET',
+                url:'https://api.parse.com/1/classes/telephone?where='+encodeURIComponent('{"name" : { "\$regex" : ".*'+q+'.*" } }'),
+                headers:
+                {
+                    'X-Parse-Application-Id': app_id,
+                    'X-Parse-REST-API-Key': api_key,
+                    'Content-Type': 'application/json'
+                }
+            })
+            .success(function(data) {
+                console.log(data);
+                callback(data);
+            });
+        },
+        getAll : function(callback){
+          console.log("GetAll");
+            $http({
+                method: 'GET',
+                url:'https://api.parse.com/1/classes/hotDeal',
+                headers:
+                {
+                    'X-Parse-Application-Id': app_id,
+                    'X-Parse-REST-API-Key': api_key,
+                    'Content-Type': 'application/json'
+                }
+            })
+            .success(function(data) {
+                console.log(data);
+                console.log("success");
+                callback(data);
+            })
+            .error(function(data){
+                console.log("error");
+            });
+
+        }
+    };
+
+});
